124237d042909c07ff2c2df73c54a65b6407e30e
runander <dcd1521@sina.com>
2017-08-28 13:38:30 +0800

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from lxml import etree
 
-
+http://www.cnblogs.com/junrong624/p/5533655.html
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'}
 url = 'https://www.amazon.cn/s/ref=amb_link_xZmRV23lO9eCAMd8396fbg_8?ie=UTF8&page=1&rh=n%3A813272051&pf_rd_m=A1AJ19PSB66TGU&pf_rd_s=merchandised-search-left-2&pf_rd_r=YNBZF5FTBS9DGBFKD6K8&pf_rd_r=YNBZF5FTBS9DGBFKD6K8&pf_rd_t=101&pf_rd_p=66d50942-1cfd-4f72-b4a3-0e56f855eca2&pf_rd_p=66d50942-1cfd-4f72-b4a3-0e56f855eca2&pf_rd_i=813108051'
 amazon_html = requests.get(url,headers = header )
